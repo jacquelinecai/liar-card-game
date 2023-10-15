1f5382fc215e95ee7b990bf97af9f96e53ff0bf8
Yuxuan Lin <yl2886@cornell.edu>
2023-10-15 15:48:40 -0400

    Implemented hand.ml and hand.mli

---
+++ src/hand.mli
@@ -0,0 +1,12 @@
+type player =
+  | Player1
+  | Player2
+  | Player3
+  | Player4
+
+type card = Card.card
+
+val player1_hand : card list
+val player2_hand : card list
+val player3_hand : card list
+val player4_hand : card list
--- src/screen.ml
+++ src/screen.ml
@@ -7,10 +7,8 @@ let main () =
   let b = W.check_box () in
   let l = W.label "Hello world" in
   let layout = L.flat_of_w [ b; l ] in
-  let size = L.set_size layout (500, 500) in
-  let background =
-    L.set_background layout (Some (L.color_bg (255, 218, 224, 255)))
-  in
+  let _ = L.set_size layout (500, 500) in
+  let _ = L.set_background layout (Some (L.color_bg (255, 218, 224, 255))) in
   let board = Bogue.of_layout layout in
   Bogue.run board
 
