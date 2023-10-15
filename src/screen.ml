bb8f8d63ae9ae9b4b0f4f9487aec9352c0769e79
Vivian Zhou <87792254+vivianzhou1288@users.noreply.github.com>
2023-10-15 18:52:52 -0400

    Finish creating the start screen

--- src/screen.ml
+++ src/screen.ml
@@ -4,11 +4,24 @@ module W = Widget
 module L = Layout
 
 let main () =
-  let button = W.button ~border_radius:10 "Start" in
+  let welcome = W.label ~size:60 "Welcome to BS" in
+  let button =
+    let button_bg = Style.Solid (181, 214, 214, 255) in
+    let button_bg_over = Some (Style.Solid (181, 214, 214, 255)) in
+    let button_bg_pressed = Style.Solid (230, 149, 151, 255) in
+    W.button ~bg_on:button_bg_pressed ~bg_over:button_bg_over ~bg_off:button_bg
+      ~border_radius:10 "Start"
+  in
   let buttonlayout = L.flat_of_w [ button ] in
-  let tower = L.tower ~sep:10 ~align:Draw.Center [ buttonlayout ] in
-  let _ = L.set_size tower (500, 500) in
-  let _ = L.set_background tower (Some (L.color_bg (255, 218, 224, 255))) in
+  let button_size = L.set_size buttonlayout (30, 20) in
+  let tower =
+    L.tower ~sep:10 ~align:Draw.Center
+      [ L.resident ~w:60 welcome; buttonlayout ]
+  in
+  let size = L.set_size tower (700, 700) in
+  let background =
+    L.set_background tower (Some (L.color_bg (255, 218, 224, 255)))
+  in
 
   (* let on_press button _ev = let b = W.get_button button in if
      Button.is_pressed b then (W.label "Hi") else (W.label "Start game") in
