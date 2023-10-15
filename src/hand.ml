b3cb909b3a618199174b1fe09ac5007981808f37
Yuxuan Lin <yl2886@cornell.edu>
2023-10-15 16:08:01 -0400

    Fixed minor mistakes in hand.ml

--- src/hand.ml
+++ src/hand.ml
@@ -6,6 +6,7 @@ type player =
 
 type card = Card.card
 
+let unshuffled_deck = Card.card_list
 let shuffled_deck = Card.shuffle Card.card_list
 
 let rec assign (a : int) (b : int) (deck : card list) (acc : card list) :
@@ -14,14 +15,15 @@ let rec assign (a : int) (b : int) (deck : card list) (acc : card list) :
   | [] -> []
   | h :: t ->
       let card_number = List.length shuffled_deck - List.length t in
-      if card_number > a && card_number < b then h :: acc else assign a b t acc
+      if card_number >= a && card_number <= b then (h :: acc) @ assign a b t acc
+      else assign a b t acc
 
 let rec order (deck : card list) : card list =
   List.sort
     (fun (s1, n1) (s2, n2) -> if n1 = n2 then compare s1 s2 else compare n1 n2)
     deck
 
-let player1_hand = assign 0 12 shuffled_deck [] |> order
-let player2_hand = assign 13 25 shuffled_deck [] |> order
-let player3_hand = assign 26 38 shuffled_deck [] |> order
-let player4_hand = assign 39 52 shuffled_deck [] |> order
+let player1_hand = assign 1 13 shuffled_deck [] |> order
+let player2_hand = assign 14 26 shuffled_deck [] |> order
+let player3_hand = assign 27 39 shuffled_deck [] |> order
+let player4_hand = assign 40 52 shuffled_deck [] |> order
--- src/hand.mli
+++ src/hand.mli
@@ -6,6 +6,11 @@ type player =
 
 type card = Card.card
 
+val shuffled_deck : card list
+
+val unshuffled_deck : card list
+(**unshuffled_deck used mainly for testing*)
+
 val player1_hand : card list
 val player2_hand : card list
 val player3_hand : card list
