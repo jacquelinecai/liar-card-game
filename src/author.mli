36f8464e23afac9f31ee03e479a3f9326502958e
Jacqueline <jxc6@cornell.edu>
2023-12-10 12:56:35 -0500

    Added author files

---
+++ src/author.mli
@@ -0,0 +1,8 @@
+(** CS 3110 Fall 2023 Final Project
+    @author Jacqueline Cai (jxc6)
+    @author Selina Lin (sxl7)
+    @author Vivian Zhou (vz48)
+    @author Yuxuan Lin (yl2886) *)
+
+val hours_worked : int
+(** [hours_worked] is the number of hours we worked on this assignment. *)
